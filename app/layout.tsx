9ff9242b65cbb8a0e3392bc90a1bff6c7978de0d
CODExxGaurav <gskaushik9050@gmail.com>
2024-12-13 17:14:13 +0530

    optimised web site for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 export default function Login({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body>
         <main>{children}</main>
       </body>
